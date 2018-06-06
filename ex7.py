f12c67a42b490d9eaaa9d9c7fddfde41ae8af245
Eieiphyu <eieiphyu7570eieiphyu@gmail.com>
2018-06-06 09:05:00 -0700

    add ex7

---
+++ ex7.py
@@ -0,0 +1,27 @@
+print("Mary had a little lamb")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." *10)#what'd thatdo?
+
+
+
+
+ee1="C"
+ee2="h"
+ee3="e"
+ee4="e"
+ee5="s"
+ee6="e"
+ee7="B"
+ee8="u"
+ee9="r"
+ee10="g"
+ee11="e"
+ee12="r"
+
+
+
+#watch ee =' ' at end.try removing it to see what happens
+print(ee1+ee2+ee3+ee4+ee5+ee6,end = ' ')
+print(ee7+ee8+ee9+ee10+ee11+ee12)
+
