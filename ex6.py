33f17fef77b115aa80d42033270084cdbd981597
Eieiphyu <eieiphyu7570eieiphyu@gmail.com>
2018-06-06 08:48:04 -0700

    add ex6

---
+++ ex6.py
@@ -0,0 +1,19 @@
+types_of_people = 10
+x = f"There are {types_of_people} types or people."
+binary = "binary"
+do_not = "don't"
+y = f"Theses who know {binary} and theses who {do_not}."
+
+print(x)
+print(y)
+
+print(f"I said: {x}.")
+print(f"I also said:{y}.")
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny!? {}"
+print(joke_evaluation.format(hilarious))
+
+w = "This is ls of ....."
+e = "a string with a rs"
+print(w + e)
