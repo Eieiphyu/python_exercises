7e120bbed09e1db2a4e72dda5ceff319c1726cbf
Eieiphyu <eieiphyu7570eieiphyu@gmail.com>
2018-06-09 08:29:35 -0700

    add ex19

---
+++ ex19.py
@@ -0,0 +1,21 @@
+def cheese_and_crackers(cheese_count,boxes_of_crackers):
+    print(f"u have {cheese_count} cheeses!")
+    print(f"u have {boxes_of_crackers} boxes of crackers!")
+    print("Men that enough for party")
+    print("Get a balance\n")
+
+print("Function numberdirectly:")
+cheese_and_crackers(20,30)
+
+print("Or use variables:")
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+
+cheese_and_crackers(amount_of_cheese,amount_of_crackers)
+    
+
+print("Do math inside 3")
+cheese_and_crackers(10+20,5+6)
+print("Ancombine variable and math:")
+cheese_and_crackers(amount_of_cheese + 100,amount_of_crackers)
