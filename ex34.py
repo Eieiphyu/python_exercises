f81c2d033c0e4c6d91e39a99ce198d6c392eeb52
Eieiphyu <eieiphyu7570eieiphyu@gmail.com>
2018-06-17 16:15:57 -0700

    add e34.py

---
+++ ex34.py
@@ -0,0 +1,11 @@
+animals = ['bear' , 'python 3.6' , 'peacock' , 'kangroo' , 'whale' , 'zebra']
+
+print(f"The animal at 1 is the 2nd animal and is a {animals[1]}.")
+print(f"The third (3rd) animal is at 2 and is a {animals[2]}.")
+print(f"The first (1st) animal is at 0 and is a {animals[0]}.")
+print(f"The animal at 3 is the forth animal and is a animal {animals[3]}.")
+print(f"The fifth (5th) animal is at 4 and is a {animals[4]}.")
+print(f"The animal at 2 is the 3rd animal and is {animals[2]}.")
+print(f"The sixth (6th) animal is at 5 and is a {animals[5]}.")
+print(f"The animal at 4 is the 5th and is a {animals[4]}.")
+
